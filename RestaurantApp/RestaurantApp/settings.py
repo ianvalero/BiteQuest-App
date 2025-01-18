66757f4c7db692ab069ff2826f77ec9307eca56c
Ian Valero <ian.gadea.ferreria@gmail.com>
2025-01-18 13:24:41 +0100

    add: Añadimos el modulo "humanize" para humanizar las fechas de creacion de comentarios y cambiamos el LANGUAGE_CODE a ingles

--- RestaurantApp/RestaurantApp/settings.py
+++ RestaurantApp/RestaurantApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'crispy_forms',
     'crispy_bootstrap5',
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'UTC'
 
