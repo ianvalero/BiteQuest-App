a8cfb93675816681e1c0eadd7b25f185696db5de
Ian Valero <ian.valero-gadea@telekom.com>
2025-02-23 13:29:27 +0100

    new: csrf_token available

--- RestaurantApp/restaurants/static/restaurants/js/restaurant_rating.js
+++ RestaurantApp/restaurants/static/restaurants/js/restaurant_rating.js
@@ -1,36 +1,40 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        let botones = document.querySelectorAll('.rating-button');
-        let rateUrl = document.querySelector('#user-rating').dataset.rateUrl;
-        console.log(rateUrl);
-        botones.forEach(function(elem) {
-            elem.addEventListener('click', function(event) {
-                rateRestaurant(
-                    rateUrl,
-                    event.target.dataset.score
-                );
-            });
+document.addEventListener('DOMContentLoaded', function() {
+    let botones = document.querySelectorAll('.rating-button');
+    let rateUrl = document.querySelector('#user-rating').dataset.rateUrl;
+    console.log(rateUrl);
+    botones.forEach(function(elem) {
+        elem.addEventListener('click', function(event) {
+            rateRestaurant(
+                rateUrl,
+                event.target.dataset.score
+            );
         });
     });
+});
 
-    function rateRestaurant(url, score) {
-        let formData = new URLSearchParams();
-        formData.append('score', score);
+function rateRestaurant(url, score) {
+    let formData = new URLSearchParams();
+    formData.append('score', score);
 
-        fetch(url, {
-            method: 'POST',
-            body: formData,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'X-CSRFToken': '{{ csrf_token }}'
-            }
-        }).then((response) => {
-            console.log(url, 'finished');
-            console.log(response);
-            if (response.ok) {
-                console.log('Url succeeded with', response);
-                location.reload();
-            }
-        }).catch((error) => {
-            alert('Url failed with '+error+' '+url);
-        })
-    };
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-CSRFToken': getCSRFToken()
+        }
+    }).then((response) => {
+        console.log(url, 'finished');
+        console.log(response);
+        if (response.ok) {
+            console.log('Url succeeded with', response);
+            location.reload();
+        }
+    }).catch((error) => {
+        alert('Url failed with '+error+' '+url);
+    })
+};
+
+function getCSRFToken() {
+    return document.querySelector('[name=csrfmiddlewaretoken]').value;
+}
