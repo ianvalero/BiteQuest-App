0156a343ac643cdfd282513b3c6865a05d7fb49d
Ian Valero <ian.valero-gadea@telekom.com>
2025-02-26 18:21:25 +0100

    add: add url for get restaurants with HTMX

--- RestaurantApp/restaurants/urls.py
+++ RestaurantApp/restaurants/urls.py
@@ -1,14 +1,14 @@
-from django.contrib.messages import success
 from django.urls import path, reverse_lazy
 from . import views
 
 app_name='restaurants'
 urlpatterns = [
     path('', views.RestaurantListView.as_view(), name='restaurant_list'),
-    path('restaurant/<slug:slug>', views.RestaurantDetailView.as_view(), name='restaurant_detail'),
+    path('restaurants', views.get_restaurants, name='get_restaurants'),
     path('restaurant/create',
          views.RestaurantCreateView.as_view(),
          name='restaurant_create'),
+    path('restaurant/<slug:slug>', views.RestaurantDetailView.as_view(), name='restaurant_detail'),
     path('restaurant/<slug:slug>/update',
          views.RestaurantUpdateView.as_view(success_url=reverse_lazy('restaurant_list')),
          name='restaurant_update'),
